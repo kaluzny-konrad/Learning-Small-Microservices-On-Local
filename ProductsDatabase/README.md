9a4d000f3b9296b6da9ce5f7a6c93854935816b8
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-07-29 18:18:13 +0200

    full test coverage

--- ProductsDatabase/README.md
+++ ProductsDatabase/README.md
@@ -31,3 +31,28 @@ $ npm run test:e2e
 # test coverage
 $ npm run test:cov
 ```
+
+## Coverage
+
+```text
+-------------------------|---------|----------|---------|---------|-------------------
+File                     | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
+-------------------------|---------|----------|---------|---------|-------------------
+All files                |     100 |      100 |     100 |     100 |
+ src                     |     100 |      100 |     100 |     100 |
+  app.module.ts          |     100 |      100 |     100 |     100 |
+  main.ts                |     100 |      100 |     100 |     100 |
+ src/prisma              |     100 |      100 |     100 |     100 |
+  prisma.module.ts       |     100 |      100 |     100 |     100 |
+  prisma.service.ts      |     100 |      100 |     100 |     100 |
+ src/products            |     100 |      100 |     100 |     100 |
+  productDto.ts          |     100 |      100 |     100 |     100 |
+  products.controller.ts |     100 |      100 |     100 |     100 |
+  products.module.ts     |     100 |      100 |     100 |     100 |
+  products.service.ts    |     100 |      100 |     100 |     100 |
+-------------------------|---------|----------|---------|---------|-------------------
+Test Suites: 5 passed, 5 total
+Tests:       31 passed, 31 total
+Snapshots:   0 total
+Time:        6.242 s
+```
--- ProductsDatabase/src/products/products.service.spec.ts
+++ ProductsDatabase/src/products/products.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaClient, Product } from '@prisma/client';
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './productDto';
+import { CreateProductDto, UpdateProductDto } from './productDto';
 import { PrismaService } from '../prisma/prisma.service';
 
 describe('ProductsService', () => {
@@ -57,6 +57,187 @@ describe('ProductsService', () => {
       expect(result).toEqual(expectedProduct);
       expect(prisma.product.create).toHaveBeenCalledWith({
         data: createDto,
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of products', async () => {
+      // Arrange
+      const expectedProducts: Product[] = [
+        {
+          id: 1,
+          name: 'Product 1',
+          description: 'Description 1',
+          price: 100,
+          imageName: 'image1.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: 'Product 2',
+          description: 'Description 2',
+          price: 200,
+          imageName: 'image2.jpg',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+
+      // Act
+      jest
+        .spyOn(prisma.product, 'findMany')
+        .mockResolvedValue(expectedProducts);
+      const result = await service.findAll();
+
+      // Assert
+      expect(result).toEqual(expectedProducts);
+      expect(prisma.product.findMany).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a product with the specified ID if it exists', async () => {
+      // Arrange
+      const productId = 1;
+      const expectedProduct: Product = {
+        id: productId,
+        name: 'Product 1',
+        description: 'Description 1',
+        price: 100,
+        imageName: 'image1.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      // Act
+      jest
+        .spyOn(prisma.product, 'findUnique')
+        .mockResolvedValue(expectedProduct);
+      const result = await service.findOne(productId);
+
+      // Assert
+      expect(result).toEqual(expectedProduct);
+      expect(prisma.product.findUnique).toHaveBeenCalledWith({
+        where: { id: productId },
+      });
+    });
+
+    it('should return null if the product with the specified ID does not exist', async () => {
+      // Arrange
+      const productId = 999;
+
+      // Act
+      jest.spyOn(prisma.product, 'findUnique').mockResolvedValue(null);
+      const result = await service.findOne(productId);
+
+      // Assert
+      expect(result).toBeNull();
+      expect(prisma.product.findUnique).toHaveBeenCalledWith({
+        where: { id: productId },
+      });
+    });
+  });
+
+  describe('findByName', () => {
+    it('should return a product with the specified name if it exists', async () => {
+      // Arrange
+      const productName = 'Product 1';
+      const expectedProduct: Product = {
+        id: 1,
+        name: productName,
+        description: 'Description 1',
+        price: 100,
+        imageName: 'image1.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      // Act
+      jest
+        .spyOn(prisma.product, 'findUnique')
+        .mockResolvedValue(expectedProduct);
+      const result = await service.findByName(productName);
+
+      // Assert
+      expect(result).toEqual(expectedProduct);
+      expect(prisma.product.findUnique).toHaveBeenCalledWith({
+        where: { name: productName },
+      });
+    });
+
+    it('should return null if the product with the specified name does not exist', async () => {
+      // Arrange
+      const productName = 'Non-existent Product';
+
+      // Act
+      jest.spyOn(prisma.product, 'findUnique').mockResolvedValue(null);
+      const result = await service.findByName(productName);
+
+      // Assert
+      expect(result).toBeNull();
+      expect(prisma.product.findUnique).toHaveBeenCalledWith({
+        where: { name: productName },
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should update the product with the specified ID and return the updated product', async () => {
+      // Arrange
+      const productId = 1;
+      const updateDto: UpdateProductDto = {
+        name: 'Updated Product Name',
+        description: 'Updated Product Description',
+        price: 150,
+        imageName: 'updated_product.jpg',
+      };
+      const expectedProduct: Product = {
+        id: productId,
+        name: updateDto.name!,
+        description: updateDto.description!,
+        price: updateDto.price!,
+        imageName: updateDto.imageName!,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      // Act
+      jest.spyOn(prisma.product, 'update').mockResolvedValue(expectedProduct);
+      const result = await service.update(productId, updateDto);
+
+      // Assert
+      expect(result).toEqual(expectedProduct);
+      expect(prisma.product.update).toHaveBeenCalledWith({
+        where: { id: productId },
+        data: updateDto,
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove the product with the specified ID and return the removed product', async () => {
+      // Arrange
+      const productId = 1;
+      const expectedProduct: Product = {
+        id: productId,
+        name: 'Product 1',
+        description: 'Description 1',
+        price: 100,
+        imageName: 'image1.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      // Act
+      jest.spyOn(prisma.product, 'delete').mockResolvedValue(expectedProduct);
+      const result = await service.remove(productId);
+
+      // Assert
+      expect(result).toEqual(expectedProduct);
+      expect(prisma.product.delete).toHaveBeenCalledWith({
+        where: { id: productId },
       });
     });
   });
