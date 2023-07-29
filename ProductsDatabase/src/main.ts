fc4c8c6b7cebf9826dc05caf8e7e3e27f671bc43
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-07-29 20:18:09 +0200

    comment swagger

--- ProductsDatabase/src/main.ts
+++ ProductsDatabase/src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Products API')
+  //   .setDescription('The Products API')
+  //   .setVersion('1.0')
+  //   .addTag('Products')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
